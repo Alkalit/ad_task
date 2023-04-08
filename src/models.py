cd7d3e2bad6daee3216e3b2ab604d4f17b54a2f9
Alkalit <lyoffa412@gmail.com>
2023-04-08 20:41:56 +0500

    Very initial integration test

--- src/models.py
+++ src/models.py
@@ -1,2 +1,24 @@
-class Campaign:
-    pass
+from dataclasses import dataclass
+from typing import NewType, Optional
+from datetime import date as pydate
+from decimal import Decimal
+
+CampaignStatId = NewType('CampaignStatId', int)
+Channel = NewType('Channel', str)
+Country = NewType('Country', str)
+Os = NewType('Os', str)
+Money = NewType('Money', Decimal)
+
+
+@dataclass()
+class CampaignStat:
+    id: Optional[CampaignStatId]
+    date: pydate
+    channel: Channel
+    country: Country
+    os: Os
+    impressions: int
+    clicks: int
+    installs: int
+    spend: Money
+    revenue: Money
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,17 @@
-from src.models import Campaign
+from httpx import Response
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
 
-def test_assert_sum():
-    assert 2 == 3
+app = FastAPI()
+
+
+class TestRootEndpoint:
+
+    def test_root_if_no_ads(self):
+        client = TestClient(app)
+        response: Response = client.get("/")
+        assert response.json() == []
+
+    def test_if_ads(self):
+        pass
