c24138593bd236c4480cda874a809079c3c51d66
Alkalit <lyoffa412@gmail.com>
2023-05-13 17:56:22 +0500

    Actually making DB queries in root endpoint.

--- api.py
+++ api.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
-
+from database import SessionFactory, Base, engine
+from db_models import CampaignStat
+from models import CampaignStat as CampaignStatSchema
 
 router = APIRouter()
+Base.metadata.create_all(bind=engine)
 
 
 @router.get("/")
-def root():
-    return []
+def root() -> list[CampaignStatSchema]:
+    session = SessionFactory()
+    stats = session.query(CampaignStat).all()
+    return stats
