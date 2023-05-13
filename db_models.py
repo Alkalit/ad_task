1daa5add52a1a1fa13d0696c831cfabd4cc62baa
Alkalit <lyoffa412@gmail.com>
2023-05-13 16:19:17 +0500

    Integrating DB and ORM into the project.

---
+++ db_models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, Date, String, Numeric
+from database import Base
+
+__all__ = ['CampaignStat']
+
+
+class CampaignStat(Base):
+    __tablename__ = 'campaign_stat'
+
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(Date, nullable=False)
+    channel = Column(String, nullable=False)
+    country = Column(String, nullable=False)
+    os = Column(String, nullable=False)
+    impressions = Column(Integer, nullable=False, default=0)
+    clicks = Column(Integer, nullable=False, default=0)
+    installs = Column(Integer, nullable=False, default=0)
+    spend = Column(Numeric, nullable=False, default="0")
+    revenue = Column(Numeric, nullable=False, default="0")
