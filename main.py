7c2547b7213de402c6836d9789a2122c00663e7c
Alkalit <lyoffa412@gmail.com>
2023-06-08 20:37:51 +0500

    Separate server and app setup logic into functions

--- main.py
+++ main.py
@@ -13,9 +13,17 @@ def setup_fastapi() -> FastAPI:
     return app
 
 
-if __name__ == '__main__':
-    Base.metadata.create_all(bind=engine)
-    app = setup_fastapi()
+def run_server(app: FastAPI) -> None:
     uvicorn_config = uvicorn.Config(app)
     server = uvicorn.Server(uvicorn_config)
     server.run()
+
+
+def main() -> None:
+    Base.metadata.create_all(bind=engine)
+    app = setup_fastapi()
+    return run_server(app)
+
+
+if __name__ == '__main__':
+    main()
