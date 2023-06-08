26c81029bfec54f86400bb445f7c7832bb3df62a
Alkalit <lyoffa412@gmail.com>
2023-06-08 20:05:29 +0500

    Manually launching the server

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from sqlalchemy.orm import Session
 
@@ -8,10 +9,13 @@ from database import Base, engine, SessionFactory
 def setup_fastapi() -> FastAPI:
     app = FastAPI()
     app.include_router(router)
+    app.dependency_overrides[Session] = lambda: SessionFactory()
     return app
 
-app = setup_fastapi()
 
-app.dependency_overrides[Session] = lambda: SessionFactory()
-
-Base.metadata.create_all(bind=engine)
+if __name__ == '__main__':
+    Base.metadata.create_all(bind=engine)
+    app = setup_fastapi()
+    uvicorn_config = uvicorn.Config(app)
+    server = uvicorn.Server(uvicorn_config)
+    server.run()
