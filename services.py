0cc32f3c77fe15241f8ffe84377e0be05550ceab
Alex <lyoffa412@gmail.com>
2023-06-11 20:36:04 +0500

    #14 specification pattern (#32)
    * Some crude attempt
    
    * Base class for specs

--- services.py
+++ services.py
@@ -1,12 +1,13 @@
 from collections.abc import Callable
-from typing import Sequence, TypeVar
+from typing import Sequence
 
-from sqlalchemy import select, null, func, asc, desc, Column, Row
+from sqlalchemy import asc, desc, Column, Row
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 
 from db_models import CampaignStat
-from models import StatOrdering, StatParams, GroupbyFields
+from models import StatOrdering, StatParams
+from specifications import StatisticSpecification, GroupBySpecification
 
 
 class Service(Callable):
@@ -24,56 +25,26 @@ class AnalyticsService(Service):
     }
 
     def __call__(self, session: Session, params: StatParams) -> Sequence[Row]:
-        if not params.groupby:
-            expression = select(
-                CampaignStat.date,
-                CampaignStat.channel,
-                CampaignStat.country,
-                CampaignStat.os,
-                CampaignStat.impressions,
-                CampaignStat.clicks,
-                CampaignStat.installs,
-                CampaignStat.spend,
-                CampaignStat.revenue,
-                (CampaignStat.spend / CampaignStat.installs).label('cpi'),
+        if params.groupby:
+            specification = GroupBySpecification(
+                date_from=params.date_from,
+                date_to=params.date_to,
+                channels=params.channels,
+                countries=params.countries,
+                os=params.os,
+                groupby=params.groupby,
             )
         else:
-            columns_with_nulls: list[Column] = []
-            columns: list[Column] = []
-            fields: dict[GroupbyFields, int] = dict(zip(params.groupby, range(len(params.groupby))))
-            for field in self.FIELDS_MAPPING:
-                if field in fields:
-                    column = self.FIELDS_MAPPING[field]
-                    columns.append(column)
-                else:
-                    column = null().label(field)
-                columns_with_nulls.append(column)
+            specification = StatisticSpecification(
+                date_from=params.date_from,
+                date_to=params.date_to,
+                channels=params.channels,
+                countries=params.countries,
+                os=params.os,
+            )
 
-            expression = select(
-                # null().label("date"),
-                # null().label("channel"),
-                # null().label("country"),
-                # null().label("os"),
-                *columns_with_nulls,
+        expression = specification()
 
-                func.sum(CampaignStat.impressions).label(CampaignStat.impressions.name),
-                func.sum(CampaignStat.clicks).label(CampaignStat.clicks.name),
-                func.sum(CampaignStat.installs).label(CampaignStat.installs.name),
-                func.sum(CampaignStat.spend).label(CampaignStat.spend.name),
-                func.sum(CampaignStat.revenue).label(CampaignStat.revenue.name),
-                (CampaignStat.spend / CampaignStat.installs).label('cpi'),
-            ).group_by(*columns)
-
-        if params.date_from:
-            expression = expression.where(CampaignStat.date >= params.date_from)
-        if params.date_to:
-            expression = expression.where(CampaignStat.date < params.date_to)
-        if params.channels:
-            expression = expression.where(CampaignStat.channel.in_(params.channels))
-        if params.countries:
-            expression = expression.where(CampaignStat.country.in_(params.countries))
-        if params.os:
-            expression = expression.where(CampaignStat.os.in_(params.os))
         if params.sort:
             field = self.FIELDS_MAPPING.get(params.sort)
             if params.ordering == StatOrdering.asc:
@@ -81,5 +52,6 @@ class AnalyticsService(Service):
             else:
                 direction = desc
             expression = expression.order_by(direction(field))
+        # CompositeSpecification.execute()
         stats = session.execute(expression).all()
         return stats
