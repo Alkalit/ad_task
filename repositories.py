2f747c00242b896bfc9d0066801b0ad705dae54c
Alkalit <lyoffa412@gmail.com>
2023-06-16 16:59:20 +0500

    Refactoring - separate work with different sql clauses.

--- repositories.py
+++ repositories.py
@@ -1,6 +1,6 @@
 from typing import Sequence, Protocol
 
-from sqlalchemy import select, Select, null, func, asc, desc, Column, Row
+from sqlalchemy import select, Select, null, func, asc, desc, Column, Row, ColumnElement
 from sqlalchemy.orm import Session
 from specifications import StatisticSpecification
 
@@ -25,6 +25,7 @@ class ICampaignStatisticsRepository(Protocol):
         ...
 
 
+# TODO rename to gateway
 class CampaignStatisticsRepository:
 
     FIELDS_MAPPING: dict[str, Column] = {
@@ -37,11 +38,18 @@ class CampaignStatisticsRepository:
     def __init__(self, session: Session):
         self._session = session
 
+    def _setup_select_clause(self, *columns: ColumnElement) -> Select:
+
+        expression = select(*columns)
+
+        return expression
+
     def _setup_sql_params(self,
                           expression: Select,
                           spec: StatisticSpecification,
                           sort: str | None = None,
                           ordering: str | None = None,
+                          groupby: list[ColumnElement] = None,
                           ) -> Select:
 
         if spec.date_from:
@@ -63,7 +71,14 @@ class CampaignStatisticsRepository:
                 direction = desc
             expression = expression.order_by(direction(field))
 
+        if groupby:
+            expression = expression.group_by(*groupby)
+
         return expression
+
+    def _execute(self, expression: Select) -> Sequence[Row]:
+        result = self._session.execute(expression).all()
+        return result
 
     def select_campaign_stats(self,
                               spec: StatisticSpecification,
@@ -71,7 +86,7 @@ class CampaignStatisticsRepository:
                               ordering: str | None = None,
                               ) -> Sequence[Row]:
 
-        expression = select(
+        expression = self._setup_select_clause(
             CampaignStat.date,
             CampaignStat.channel,
             CampaignStat.country,
@@ -86,7 +101,7 @@ class CampaignStatisticsRepository:
 
         expression = self._setup_sql_params(expression, spec, sort, ordering)
 
-        stats = self._session.execute(expression).all()
+        stats = self._execute(expression)
         return stats
 
     def select_campaign_analytical_stats(self,
@@ -96,33 +111,28 @@ class CampaignStatisticsRepository:
                                          ) -> Sequence[Row]:
 
         columns_with_nulls: list[Column] = []
-        columns: list[Column] = []
+        groupby_columns: list[Column] = []
         fields: dict[GroupbyFields, int] = dict(zip(spec.groupby, range(len(spec.groupby))))
 
         for field in self.FIELDS_MAPPING:
             if field in fields:
                 column = self.FIELDS_MAPPING[field]
-                columns.append(column)
+                groupby_columns.append(column)
             else:
                 column = null().label(field)
             columns_with_nulls.append(column)
 
-        expression = select(
-            # null().label("date"),
-            # null().label("channel"),
-            # null().label("country"),
-            # null().label("os"),
+        expression = self._setup_select_clause(
             *columns_with_nulls,
-
             func.sum(CampaignStat.impressions).label(CampaignStat.impressions.name),
             func.sum(CampaignStat.clicks).label(CampaignStat.clicks.name),
             func.sum(CampaignStat.installs).label(CampaignStat.installs.name),
             func.sum(CampaignStat.spend).label(CampaignStat.spend.name),
             func.sum(CampaignStat.revenue).label(CampaignStat.revenue.name),
             (CampaignStat.spend / CampaignStat.installs).label('cpi'),
-        ).group_by(*columns)
+        )
 
-        expression = self._setup_sql_params(expression, spec, sort, ordering)
+        expression = self._setup_sql_params(expression, spec, sort, ordering, groupby_columns)
 
-        stats = self._session.execute(expression).all()
+        stats = self._execute(expression)
         return stats
