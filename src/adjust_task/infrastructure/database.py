25835743d849aaf80f7f23d148619d2f59e573b9
Alkalit <lyoffa412@gmail.com>
2023-06-26 20:36:34 +0500

    Setting path to the production database.

--- src/adjust_task/infrastructure/database.py
+++ src/adjust_task/infrastructure/database.py
@@ -1,11 +1,21 @@
+from pathlib import Path
 from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app.sqlite3"
+this_file = Path(__file__)
+project_dir = this_file.parent.parent.parent.parent
+db_path = project_dir / 'app.sqlite3'
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{db_path}"
 
 
 def setup_engine() -> Engine:
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=dict(check_same_thread=False))
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        connect_args=dict(check_same_thread=False),
+        echo=True,
+        echo_pool="debug",
+    )
     return engine
 
 
