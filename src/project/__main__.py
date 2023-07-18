236308ce7187ab9342505d2b4b0a6c3057a3920a
Alkalit <lyoffa412@gmail.com>
2023-07-18 16:26:54 +0500

    Application server is now asynchronous.

--- src/project/__main__.py
+++ src/project/__main__.py
@@ -1,3 +1,4 @@
+from asyncio import run
 from sqlalchemy.orm import Session
 
 from project.infrastructure.framework import setup_api
@@ -5,15 +6,15 @@ from project.infrastructure.database import setup_engine, setup_session
 from project.infrastructure.server import run_server
 
 
-def main() -> None:
+async def main() -> None:
     engine = setup_engine()
     session_factory = setup_session(engine)
 
     app = setup_api()
     app.dependency_overrides[Session] = lambda: session_factory()
 
-    return run_server(app)
+    await run_server(app)
 
 
 if __name__ == '__main__':
-    main()
+    run(main())
--- src/project/infrastructure/server.py
+++ src/project/infrastructure/server.py
@@ -2,7 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 
 
-def run_server(app: FastAPI) -> None:
+async def run_server(app: FastAPI) -> None:
     uvicorn_config = uvicorn.Config(app)
     server = uvicorn.Server(uvicorn_config)
-    server.run()
+    await server.serve()
