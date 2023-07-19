488020df1da296b8a0fd74c68e58225ac0cec126
Alex <lyoffa412@gmail.com>
2023-07-19 14:07:07 +0300

    Async (#49)
    * Application server is now asynchronous.
    
    * Framework layer is async
    
    * Application layer is async
    
    * Application layer is async
    
    * fix

--- src/project/__main__.py
+++ src/project/__main__.py
@@ -1,19 +1,19 @@
-from sqlalchemy.orm import Session
+from asyncio import run
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from project.infrastructure.framework import setup_api
 from project.infrastructure.database import setup_engine, setup_session
 from project.infrastructure.server import run_server
 
 
-def main() -> None:
+async def main() -> None:
     engine = setup_engine()
     session_factory = setup_session(engine)
 
     app = setup_api()
-    app.dependency_overrides[Session] = lambda: session_factory()
-
-    return run_server(app)
+    app.dependency_overrides[AsyncSession] = lambda: session_factory()
+    await run_server(app)
 
 
 if __name__ == '__main__':
-    main()
+    run(main())
