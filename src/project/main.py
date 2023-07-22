21791739ed746117b8874bc1b1f58cbc7362693d
alkalit <lyoffa412@gmail.com>
2023-07-22 18:09:45 +0500

    Clean architecture version

--- src/project/main.py
+++ src/project/main.py
@@ -1,23 +1,26 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Callable
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import FastAPI
 
 from project.infrastructure.framework import setup_api
 from project.infrastructure.database import setup_engine, setup_session
 
-# def session_manager(session_factory: Callable[[], AsyncSession])
+
+def session_manager(
+        session_factory: Callable[[], AsyncSession]
+) -> Callable[[], AsyncGenerator[AsyncSession, None]]:
+
+    async def wrapper() -> AsyncGenerator[AsyncSession, None]:
+        session = session_factory()
+        yield session
+        await session.close()
+    return wrapper
 
 
 def main() -> FastAPI:
     engine = setup_engine()
     session_factory = setup_session(engine)
 
-    async def session_manager() -> AsyncGenerator[AsyncSession, None]:
-        session = session_factory()
-        yield session
-        await session.close()
-
     app = setup_api()
-    # app.dependency_overrides[AsyncSession] = lambda: session_factory()
-    app.dependency_overrides[AsyncSession] = session_manager
+    app.dependency_overrides[AsyncSession] = session_manager(session_factory)
     return app
